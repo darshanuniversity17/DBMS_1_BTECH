fa69ff76e2fae6e7fd18bb0846a627dd98f982d4
Om <codersarea12@gmail.com>
2025-06-16 18:59:04 +0530

    2 and 4

---
+++ schema/lab4.sql
@@ -0,0 +1,29 @@
+--Part � A:
+
+CREATE TABLE CRICKET (
+    Name VARCHAR(50),
+    City VARCHAR(50),
+    Age INT
+);
+
+INSERT INTO CRICKET VALUES
+('Sachin Tendulkar', 'Mumbai', 30),
+('Rahul Dravid', 'Bombay', 35),
+('M. S. Dhoni', 'Jharkhand', 31),
+('Suresh Raina', 'Gujarat', 30)
+
+
+--Part � B:
+
+
+CREATE TABLE EMPLOYEE (
+    Name VARCHAR(50),
+    City VARCHAR(50),
+    Age INT
+);
+
+INSERT INTO EMPLOYEE VALUES
+('Jay Patel', 'Rajkot', 30),
+('Rahul Dave', 'Baroda', 35),
+('Jeet Patel', 'Surat', 31),
+('Vijay Raval', 'Rajkot', 30)
